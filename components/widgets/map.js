3d2d7e204c0df1edab7d19c9939239260e3bfb84
Michael Gardner <Mgardner3000@gmail.com>
2024-10-02 21:21:50 -0700

    Weather stations mapped on a state (Lots of fiddling)

--- components/widgets/map.js
+++ components/widgets/map.js
@@ -6,49 +6,74 @@ class WorldMap extends HTMLElement {
 
         const shadow = this.attachShadow({ mode: "open" })
         const map = document.createElement("div")
+        map.style.height = "100%"
+        map.style.width = "100%"
         map.id = this.getAttribute("id")
         shadow.appendChild(map)
     }
 
     connectedCallback() {
+        fetch("https://api.weather.gov/stations?state=AZ").then(result => result.json()).then(result => {
+            console.log(result)
+            fetch('../../static/data/world/countries/USA/AZ.geo.json').then(res => res.json()).then(res => {
+                console.log(res)
 
-        fetch('../../static/data/lower48.json').then(res => res.json()).then(res => {
-            const source = new ol.source.Vector({
-                format: new ol.format.GeoJSON({ projection: 'EPSG:3857', }),
-                url: '../../static/data/lower48.json',
-            })
-            const extent = ol.proj.transformExtent(new ol.source.Vector({
-                features: new ol.format.GeoJSON().readFeatures(res)
-              }).getExtent(), 'EPSG:4326', 'EPSG:3857');
-              console.log(extent)
-            const map = new ol.Map({
-                target: this.shadowRoot.querySelector("#" + this.getAttribute("id")),
-                layers: [
-                    new ol.layer.Vector({
-                        source: source
+                const stateFeatures = new ol.format.GeoJSON().readFeatures(res, { featureProjection: 'EPSG:3857' })
+                const stateFeaturesCollection = new ol.Collection(stateFeatures)
+                const source = new ol.source.Vector({ features: stateFeaturesCollection })
+
+                const stationFeatures = new ol.format.GeoJSON().readFeatures(result, {dataProjection: 'EPSG:4326',  featureProjection: 'EPSG:3857' })
+                const stationCollection = new ol.Collection(stationFeatures)
+                const stationSource = new ol.source.Vector({ features: stationCollection })
+                console.log(stationSource)
+                const stationVector = new ol.layer.Vector({
+                    source: stationSource,
+                    style: new ol.style.Style({
+                        image: new ol.style.Circle({
+                            radius: 3,
+                            fill: new ol.style.Fill({color: 'black'})
+                          })
                     }),
-                ],
-                view: new ol.View({
-                    center: ol.extent.getCenter(extent),
-                    zoom: 3,
-                    minZoom: 3,
-                    extent: extent
-                }),
-            });
-    
-    
-    
-            map.on('click', function (evt) {
-                console.log(ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'));
-            });
-    
-    
-            map.render();
+                })
+
+                console.log(stationVector)
+
+                const Vector = new ol.layer.Vector({
+                    source: source,
+                    style: new ol.style.Style({
+                        fill: new ol.style.Fill({
+                            color: 'red',
+                        }),
+                        stroke: new ol.style.Stroke({
+                            color: 'white',
+                        }),
+                    }),
+                })
+
+                const map = new ol.Map({
+                    target: this.shadowRoot.querySelector("#" + this.getAttribute("id")),
+                    layers: [
+                        Vector,
+                        stationVector
+                    ],
+                    view: new ol.View({
+                        center: ol.extent.getCenter(source.getExtent()),
+                        zoom: 6,
+                        // minZoom: 6,
+                        // maxZoom: 6,
+                        extent: source.getExtent(),
+                        // projection: 'EPSG:4326'
+                    })
+                });
+
+                map.on('click', function (evt) {
+                    console.log(ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'));
+                });
+
+                map.render();
+            })
         })
-
-
     }
-
 
     adoptedCallback() {
         console.log("Custom element moved to new page.");
